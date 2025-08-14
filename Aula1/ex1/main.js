0d281f2dc25295127a50ec928d54cd7938f6d18c
Ivan Pires <ivanpires@gmail.com>
2025-08-14 08:47:47 -0400

    tmp files

--- Aula1/ex1/main.js
+++ Aula1/ex1/main.js
@@ -1,37 +1,11 @@
-// ### Declaração de Variávei
-
-// Usando let (pode ser alterada depois)
-let nome = "Maria";
-nome = "João"; // valor alterado
-
-// Usando const (não pode ser alterada)
-const idade = 25;
-// idade = 30; // Isto causaria um erro
-
-// Usando var (forma antiga, evite usar)
-var cidade = "Cuiabá";
-
-console.log(`Nome: ${nome}, Idade: ${idade}, Cidade: ${cidade}`);
-
-// ### Arrow Functions
-
-// Função tradicional
-function soma(a, b) {
-    return a + b;
-}
-
-// Arrow function equivalente
-const somaArrow = (a, b) => a + b;
-
-// Arrow function com apenas um parâmetro
-const dobro = x => x * 2;
-
-// Arrow function sem parâmetros
-const saudacao = () => console.log("Olá, mundo!");
-
-// Exemplos de uso
-console.log(soma(2, 3));        // 5
-console.log(somaArrow(4, 7));   // 11
-console.log(dobro(10));         // 20
-saudacao();                     // Olá, mundo!
-
+/**
+ * Exemplo 1: Declaração de variáveis e funções em JavaScript.
+ *
+ * Conteúdo:
+ * - Diferenças entre let, const e var para declaração de variáveis.
+ * - Alteração de valores em variáveis declaradas com let.
+ * - Imutabilidade de variáveis declaradas com const.
+ * - Uso de funções tradicionais e arrow functions.
+ * - Exemplos de arrow functions com 2+, 1 e 0 parâmetros.
+ *
+ */
